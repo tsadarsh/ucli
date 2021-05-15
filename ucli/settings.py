3260f43da660837486effa9e0541fef3de006549
Ubuntu <tsadarsh-ucli@ucli-trial.yrq4ymj1yeoeveigf5zs13orcf.bx.internal.cloudapp.net>
2021-05-15 06:07:01 +0000

    moved secret key to root dir

--- ucli/settings.py
+++ ucli/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
-with open('ucli/django-secret', 'r') as f:
+with open('django-secret.txt', 'r') as f:
     SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
